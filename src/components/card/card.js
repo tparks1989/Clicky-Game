1381289a67316a1cf66f72c45f2f62c542919201
TP <tparks1989@yahoo.com>
2019-06-04 23:09:43 -0400

    three

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,25 +1,14 @@
+
 import React from "react";
-import "./card.css";
+import "./Card.css";
 
-const Card = props => ( <
-    div className = "card img-container hover" >
-    <
-    img alt = {
-        props.name
-    }
-    src = {
-        props.image
-    }
-    id = {
-        props.id
-    }
-    onClick = {
-        () => props.shuffleScoreCard(props.id)
-    }
-    className = 'shuffleScore' / >
-
-    <
-    /div>
+//this component is only rendering the images to the screen and can be a simple render function it is a
+//stateless component
+const Card = props => (
+  <div className="card img-container hover">
+      <img alt={props.name} src={props.image} id={props.id}
+        onClick={() => props.shuffleScoreCard(props.id)} className='shuffleScore'/>
+  </div>
 );
 
 export default Card;
