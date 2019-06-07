660138b52267b5444e74139d2b29614c67857e14
TP <tparks1989@yahoo.com>
2019-06-07 00:32:13 -0400

    last

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
         />
         <footer className="footer">
       <div className="container">
-        <span className="text-muted">&copy;Tyrone - Clicky Game - React app.</span>
+        <span className="text-muted">&copy;Tyrone Clicky-Game</span>
       </div>
     </footer> 
       </div>
